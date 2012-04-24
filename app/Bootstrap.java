c9585c5bc9ec9c7b4753d1d90d15fa8f150deb90
Jasdeep Madan <jasdeepm@gmail.com>
2012-04-25 00:20:05 +0530

    fixed Ip-Address issue in initial data load

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -14,7 +14,7 @@ public class Bootstrap extends Job{
             System.out.println("U Got No Data!");
         }
         if (User.count() == 0){
-            new User("Jasdeep","Madan","JD","jasdeepm@gmail.com","98036054","theboss",true,true).save();
+            new User("Jasdeep","Madan","JD","jasdeepm@gmail.com","98036054","theboss",true,true, "127.0.0.1").save();
             Fixtures.loadModels("initial-data.yml");
         }
         
