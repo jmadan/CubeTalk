aca538a375145c58acfab1af3f56db14575acfa5
Jasdeep Madan <jasdeepm@gmail.com>
2012-04-28 18:58:57 +0530

    removing user from Bootstrap

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -9,15 +9,8 @@ public class Bootstrap extends Job{
 
     public void doJob() {
         // Check if the database is empty
-        if(Article.count() == 0) {
-//            Fixtures.loadModels("initial-data.yml");
-            System.out.println("U Got No Data!");
-        }
-        if (User.count() == 0){
-            new User("Jasdeep","Madan","JD","jasdeepm@gmail.com","98036054","theboss",true,true, "127.0.0.1").save();
             Fixtures.loadModels("initial-data.yml");
-        }
-        
+
     }
 
 
